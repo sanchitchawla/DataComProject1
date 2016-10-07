5f1c55bf3bad46fffdc228b557349d09a3397bbb
sanchitchawla <sanchit_sid@hotmail.com>
2016-10-07 18:11:35 +0700

    Add files via upload
    First File Upload
    1.0

---
+++ project1.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import socket as s
+from urlparse import urlparse
+
+fileName= "working on it"
+HOST = hostpath("working on it")[0]
+PATH = hostpath("working on it")[1]
+
+def network():
+	text = ""
+	res = connection.recv(1024)
+	while len(res) != 0:
+		text += res
+		res = connection.recv(1024)
+	return text
+
+def connect(HOST,PORT):
+	try:
+		clientSocket = soc.socket(soc.AF_INET, soc.SOCK_STREAM)
+		clientSocket.connect((HOST, PORT))
+		connection = clientSocket
+		return connection
+	except Exception, e:
+		"Something is wrong, Windows Style"
+	else:
+		print "Can't connect"
+
+
+def hostpath(url):
+	work = urlparse(url)
+	ls = []
+	ls.append(work.host) # at index 0 we append the host
+	path= work.path
+	if work.query!="":
+		path = path + "?" + query
+	ls.append(path) # at index 1 we append the path + query if there is one
+	return ls  #return a list of [host,path]
+
+def getPort(url):
+	work = urlparse(url)
+	return work.port # well, gets the port number
+
+def downloadrqt(host,path,GETHEAD):
+	if GET:
+		getorhead = "GET"
+	else : getorhead = "HEAD"
+	return getorhead + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n\r\n"
+
+def continuerqt(host,path,date,lastStop):
+	return "GET " + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "If-Range: " + date + "Range: " + "bytes=" + lastStop + "-" + "\r\n\r\n"
+
+
+connection=connect(HOST,PORT)
+
+
+with open(fileName, "wb") as file:
+	file.write(content)
+	
