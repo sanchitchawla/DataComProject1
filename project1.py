12054eadeb94d1027fd83836b0759b5cc06bed62
Sanchit Chawla <sanchit_sid@hotmail.com>
2016-10-10 21:58:17 +0700

    Got the thing to work with just GET

--- project1.py
+++ project1.py
@@ -6,30 +6,7 @@ import argparse
 import sys
 
 def downloadrqt(host,path,GETHEAD):
-	if GETHEAD:
-		getorhead = "GET "
-	else : getorhead = "HEAD "
-	return getorhead + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n\r\n"
-
-def getlength(HOST,PORT,PATH):
-	clientSocket = soc.socket(soc.AF_INET, soc.SOCK_STREAM)
-	clientSocket.connect((HOST,PORT))
-	print "connected"
-	header = downloadrqt(HOST,PATH,False)
-	clientSocket.send(header)
-	text = ""
-	res = clientSocket.recv(1024)
-	cont_len=""
-	while len(res) != 0:
-		text += res
-		res = clientSocket.recv(1024)
-		for i in text[text.find("Content-Length") + 16:]:
-			if i =="\r":
-				break
-			cont_len+=i
-	cont_len=int(cont_len)
-	clientSocket.close()
-	return cont_len
+	return "GET " + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n\r\n"
 
 def downloadpls(HOST,PORT,PATH):
 	clientSocket = soc.socket(soc.AF_INET, soc.SOCK_STREAM)
@@ -37,28 +14,30 @@ def downloadpls(HOST,PORT,PATH):
 	print "connected again"
 	header = downloadrqt(HOST,PATH,True)
 	clientSocket.send(header)
-	text = ""
+	text = "" 
 	res = clientSocket.recv(1024)
-	cont_len=""
+	cont_len=[]
 	while len(res) != 0:
 		text += res
 		res = clientSocket.recv(1024)
 	lis= text.split("\r\n\r\n")
-	return lis[1]
+	cont_len= lis[0].split("\r\n")
+	final= cont_len[-3].split(": ")
+	return lis[1], final[1]
 
 if len(sys.argv) ==4 and sys.argv[1]== "-o" or len(sys.argv) == 6 and sys.argv[-3]== "-c":
 	if "https" in sys.argv[-1]:
 		print "Sorry we don't support https :/"	
 		sys.exit(2)
+	elif "http" not in sys.argv[-1]:
+		print "is your http correct?"
 	parseSTR = urlparse(sys.argv[-1])
 	if parseSTR.port == None: PORT = 80
 	else: PORT = parseSTR.port
 	fileName=sys.argv[2]
 	netloc = parseSTR.hostname
-	print netloc
 	path = parseSTR.path
-	length=getlength(netloc, PORT, path)
-	text=downloadpls(netloc,PORT,path)
+	text,length=downloadpls(netloc,PORT,path)h
 	with open("/home/sanchit/Desktop/"+ fileName, "wb+") as file:
 		file.write(text)
 	
