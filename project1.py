1d48c2e4b09bdd5390480c1669f531c38205cf81
Sanchit Chawla <sanchit_sid@hotmail.com>
2016-10-11 18:07:14 +0700

    Waiting for content length to work

--- project1.py
+++ project1.py
@@ -13,11 +13,11 @@ def isRedirect(HOST,PORT,PATH):
 	header = downloadrqt(HOST,PATH)
 	clientSocket.send(header)
 	text = "" 
-	res = clientSocket.recv(8096)
+	res = clientSocket.recv(4096)
 	cont_len=[]
 	while len(res) != 0:
 		text += res
-		res = clientSocket.recv(8096)
+		res = clientSocket.recv(4096)
 	lis= text.split("\r\n\r\n")
 	ol= lis[0].split("\r\n")
 	if "301" in ol[0] or "302" in ol[0]:
@@ -35,17 +35,46 @@ def isRedirect(HOST,PORT,PATH):
 	header = downloadrqt(HOST,PATH)
 	clientSocket.send(header)
 	text = "" 
-	res = clientSocket.recv(8096)
-	cont_len=[]
+	res = clientSocket.recv(1024)
+	count=1
 	while len(res) != 0:
 		text += res
-		res = clientSocket.recv(8096)
+		res = clientSocket.recv(1024)
+		if "Content-Length" in text:
+			
+			# we=text.split("\r\n\r\n")
+			# hehe=we[0].split("\r\n")
+			# sixth= hehe[6].split(": ")
+			# seventh=hehe[7].split(": ")
+			# if sixth[1].isalpha()==False:
+			# 	length= int(sixth[1])
+			# else: length= int(seventh[1])
+		downloaded= 1024 * count
+		print (float(downloaded) / float(length)) * 100
+		count+=1
+		if float(downloaded) == float(length):
+			break
+	print "Download Complete!"
 	lis= text.split("\r\n\r\n")
-	print lis[0]
-	cont_len= lis[0].split("\r\n")
-	final= cont_len[-3].split(": ")
-	return lis[1], final[1]
+	return lis[1]
 
+
+if len(sys.argv) ==4 and sys.argv[1]== "-o":
+	if "https" in sys.argv[-1]:
+		print "Sorry we don't support https"	
+		sys.exit(2)
+	parseSTR = urlparse(sys.argv[-1])
+	if parseSTR.port == None: PORT = 80
+	else: PORT = parseSTR.port
+	fileName=sys.argv[2]
+	HOST = parseSTR.hostname
+	PATH = parseSTR.path
+	if PATH=="" or PATH== None:
+		PATH="/"
+	text=isRedirect(HOST,PORT,PATH)
+	with open("/home/sanchit/Desktop/"+ fileName, "wb+") as file:
+		file.write(text)
+	
 # def downloadpls(HOST,PORT,PATH):
 # 	clientSocket = soc.socket(soc.AF_INET, soc.SOCK_STREAM)
 # 	clientSocket.connect((HOST,PORT))
@@ -61,23 +90,3 @@ def isRedirect(HOST,PORT,PATH):
 # 	cont_len= lis[0].split("\r\n")
 # 	final= cont_len[-3].split(": ")
 # 	return lis[1], final[1]
-
-if len(sys.argv) ==4 and sys.argv[1]== "-o":
-	if "https" in sys.argv[-1]:
-		print "Sorry we don't support https :/"	
-		sys.exit(2)
-	parseSTR = urlparse(sys.argv[-1])
-	if parseSTR.port == None: PORT = 80
-	else: PORT = parseSTR.port
-	fileName=sys.argv[2]
-	HOST = parseSTR.hostname
-	PATH = parseSTR.path
-	if PATH=="" or PATH== None:
-		PATH="/"
-	#isRedirect(HOST,PORT,PATH)
-	text,length=isRedirect(HOST,PORT,PATH)
-	if type(length)!= int:
-		print "Cannot fetch Content- Length"
-	with open("/home/sanchit/Desktop/"+ fileName, "wb+") as file:
-		file.write(text)
-	
